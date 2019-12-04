25220016c7527daa7c3b331b718372fd6b1916f9
wuminh <597950687@qq.com>
2019-12-04 10:30:34 +0800

    eureka设置验证

--- services/eureka/src/main/java/clould4myself/com/eureka/EurekaApplication.java
+++ services/eureka/src/main/java/clould4myself/com/eureka/EurekaApplication.java
@@ -3,11 +3,28 @@ package clould4myself.com.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class,args);
+    }
+
+    /**
+     * eureka注册中心开启security密码验证时需要
+     */
+    @EnableWebSecurity
+    class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity httpSecurity) throws Exception {
+            // Spring Security 默认开启了所有 CSRF 攻击防御，需要禁用 /eureka 的防御
+            httpSecurity.csrf().ignoringAntMatchers("/eureka/**");
+
+            super.configure(httpSecurity);
+        }
     }
 }
